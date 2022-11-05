14fbd4c7ee067030d43f83e6b37f9d0d0a78148e
Thamyris <thamyriscabral@hotmail.com>
2022-11-05 11:33:02 -0300

    fim

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-function name (nome) {
-  const nomeUsuario = 'Thamyris'
-  //document.querySelector('.top-bar p').textContent = 'Bem vindo(a), ' + nomeUsuario
-  const elemento = document.querySelector('.top-bar p')
-  //elemento.textContent = 'Bem vindo(a), ' + nomeUsuario
-  elemento.textContent = elemento.textContent + nomeUsuario // isso aqui poderia ser elemento.textContent += nomeUsuario
-}
-name() 
+(function () {
+  //const elemento = document.querySelector('.top-bar p')
+  const nomeUsuario = 'thamyris'
+  if(nomeUsuario){
+    const addNome = document.createElement('div')
+    addNome.className = 'top-bar'
+    addNome.innerHTML = ` <p> Bem vindo, ${nomeUsuario} </p>`
+    const elementoPai = document.querySelector('.hero')
+    elementoPai.insertBefore(addNome, elementoPai.firstElementChild)
+  }
+})()
+  
+  
+  
+  
+  
+  
+    //document.querySelector('.top-bar p').textContent = 'Bem vindo(a), ' + nomeUsuario
+  //elemento.innerHTML = 'Bem vindo(a), ' + nomeUsuario
+//   elemento.innerHTML = elemento.innerHTML + '<br>' + nomeUsuario + '</br>' // isso aqui poderia ser elemento.innerHTML += nomeUsuario
+//   }
+//   else {
+//     const removeFilho = elemento.parentElement
+//     removeFilho.parentElement.removeChild(removeFilho)
+//   }
+// }
+// name()
